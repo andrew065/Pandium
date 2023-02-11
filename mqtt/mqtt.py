027f418b6b28e796d3387fabed993f406c4103c2
andrew065 <andrewlian345@gmail.com>
2023-02-11 14:44:01 -0500

    finished mqtt integration

--- mqtt/mqtt.py
+++ mqtt/mqtt.py
@@ -1,22 +1,36 @@
+import time
+import certifi
 import paho.mqtt.client as mqtt
 import creds
-import ssl
-
-client = mqtt.Client(client_id='hos1', clean_session=False)
-
-client.username_pw_set(username=creds.USERNAME, password=creds.PASSWORD)
-client.connect('tcps://mr-connection-brdco6qtfz5.messaging.solace.cloud:8443', port=8080, keepalive=60, bind_address="")
-client.loop_start()
-
-client.publish('my/topic', 'hello world')
-client.subscribe('my/topic')
 
 
-def on_message(client, usr_data, msg):
-    print(f'Received message on topic: {msg.topic}. Message: {msg.payload.decode("utf-8")}')
+class SolaceClient:
+    def __init__(self, name):
+        self.name = name
 
 
+# Callback on connection
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected (Result: {rc})')
+    client.subscribe('my/topic')
+
+
+def on_message(client, userdata, msg):
+    print(f'Message received on topic: {msg.topic}. Message: {msg.payload}')
+
+
+client = mqtt.Client(transport='websockets')
+client.on_connect = on_connect
 client.on_message = on_message
 
+client.tls_set(ca_certs=certifi.where())
+
+client.username_pw_set('solace-cloud-client', creds.PASSWORD)
+client.connect('mr-connection-brdco6qtfz5.messaging.solace.cloud', port=8443)
+
+client.loop_start()
+
 while True:
-    pass
+    client.publish('my/topic', 'hello world')
+
+    time.sleep(1.0)
