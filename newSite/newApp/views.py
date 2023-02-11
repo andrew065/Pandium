ac4555054c2f0ef36bd113f16533f7a2118ad84d
lemon-exe <120424610+lemon-exe@users.noreply.github.com>
2023-02-11 17:42:20 -0500

    Updated files for hospital frontend

--- newSite/newApp/views.py
+++ newSite/newApp/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import render
 import paho.mqtt.client as mqtt
 import certifi
 from django.http import HttpResponse
-
+import random as rand
+import csv
+msgO = ""
+cnt = 0
 def on_connect(client, userdata, flags, rc):
+
     print("Connected with result code "+str(rc))
-    print("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-    client.subscribe("my/topic")
+    client.subscribe("+/topic")
 
 def on_message(client, userdata, msg):
+    msgO = str(msg.payload)
     print(msg.topic+" "+str(msg.payload))
 
 def receive_messages(request):
+
     client = mqtt.Client(transport = "websockets")
     client.on_connect = on_connect
 
@@ -27,4 +32,6 @@ def receive_messages(request):
     print("connected                 ------")
     client.loop_start()
     #return render(request, 'index.html')
-    return HttpResponse("Connected to MQTT broker.")
+
+    return HttpResponse(rand.random())
+
