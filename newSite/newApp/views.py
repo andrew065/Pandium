972675d3e8705636bb566515cde77e804072b5a7
lemon-exe <120424610+lemon-exe@users.noreply.github.com>
2023-02-11 15:06:43 -0500

    Updated files for hospital frontend

--- newSite/newApp/views.py
+++ newSite/newApp/views.py
@@ -5,20 +5,22 @@ from django.http import HttpResponse
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("mqtt_topic")
+    print("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    client.subscribe("topic")
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 def receive_messages(request):
-    client = mqtt.Client()
+    client = mqtt.Client(transport = "websockets")
     client.on_connect = on_connect
+
     print("connected")
     client.on_message = on_message
     print("messaged")
     client.username_pw_set("solace-cloud-client", "llgr44bsrp3qn6jq54689ke2p9")
     print("up set")
-    client.connect("172.19.0.2", 8000, 60)
+    client.connect('mr-connection-brdco6qtfz5.messaging.solace.cloud', port=8443)
     print("connected                 ------")
     client.loop_start()
     #return render(request, 'index.html')
