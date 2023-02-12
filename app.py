f70b5c1d05e1b1f594572d36423a15f0ecdae55c
andrew065 <andrewlian345@gmail.com>
2023-02-11 23:03:24 -0500

    flask setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 import sqlite3
 import os
 
@@ -9,14 +9,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/ambulance')
+@app.route('/ambulance', methods=['POST', 'GET'])
 def ambulance():
-    return render_template('index_ambu.html')
+    if request.method == 'POST':
+        request.form
+        print('button clicked')
+        return render_template('index_ambu.html')
+    else:
+        return render_template('index_ambu.html')
 
 
 @app.route('/hospital')
 def hospital():
     return render_template('index_hosp.html')
+
+
+@app.route('/publish')
+def publish():  # make endpoint and fetch during button press
+    print('button clicked')
 
 
 if __name__ == '__main__':
